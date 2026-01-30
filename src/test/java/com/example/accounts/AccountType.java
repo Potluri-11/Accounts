8b94159cb0cffb3b8a53c80d03115b242d18f819
Potluri-11 <srujanapotluri43@gmail.com>
2026-01-30 13:12:40 +0530

    Update and rename account-type.java to AccountType.java

--- src/test/java/com/example/accounts/AccountType.java
+++ src/test/java/com/example/accounts/AccountType.java
@@ -3,7 +3,7 @@ package com.example.accounts;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class Account-typeTest {
+class AccountTypeTest {
 
     @Test
     void premiumAccount() {
